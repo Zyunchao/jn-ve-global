fe9350e53c118a9314855eeab9baa7c7843cd973
Zhangyunchao <18651805393@163.com>
2022-01-05 12:17:27 +0800

    暴露 gchart 的接口；

---
+++ packages/GChart/index.ts
@@ -0,0 +1,3 @@
+export { default as InitOpts } from './interface/InitOpts'
+export { default as ChartConfig } from './interface/ChartConfig'
+export { default as BarOrLine } from './interface/BarOrLine'
