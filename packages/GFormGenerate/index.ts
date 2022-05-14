b3b74adfd399b0304bb176dd550ea3245a2b282b
Zyunchao <18651805393@163.com>
2022-05-14 18:31:44 +0800

    更新日志；

--- packages/GFormGenerate/index.ts
+++ packages/GFormGenerate/index.ts
@@ -1,3 +1,5 @@
 export * from './interface/FormGenerateProps'
+export * from './implements/AdvanceFormConfig'
 
 export { default as FormGenerateProps } from './interface/FormGenerateProps'
+export { default as AdvanceFormConfig } from './implements/AdvanceFormConfig'
