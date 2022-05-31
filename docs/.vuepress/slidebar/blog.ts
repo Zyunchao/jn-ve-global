db6de5b494701aeb6e605dd00d9725d9fee71ef1
Zyunchao <18651805393@163.com>
2022-05-31 11:10:20 +0800

    调整目录

--- docs/.vuepress/slidebar/blog.ts
+++ docs/.vuepress/slidebar/blog.ts
@@ -4,11 +4,16 @@ export default [
         link: '/blog/index.md'
     },
     {
-        text: '熟悉而陌生的模块化',
-        link: '/blog/熟悉而陌生的模块化.md'
-    },
-    {
-        text: '嗨，你知道 let 和 const 吗？',
-        link: '/blog/letconst.md'
+        text: '原生 JS',
+        children: [
+            {
+                text: '嗨，你知道 let 和 const 吗？',
+                link: '/blog/letconst.md'
+            },
+            {
+                text: '熟悉而陌生的模块化',
+                link: '/blog/熟悉而陌生的模块化.md'
+            }
+        ]
     }
 ]
