3e17d753e878fdfc46f01eeceacd87f86a900189
Zyunchao <18651805393@163.com>
2022-10-09 09:27:39 +0800

    test: 测试

--- packages/index.ts
+++ packages/index.ts
@@ -10,7 +10,7 @@ export * from './JnEditor/interface/tinymce'
 export { default as InfoColumnProps } from './GInfoSA/interface/InfoColumnProps'
 export { default as IrregularTableCell } from './GIrregularTable/interface/Cell'
 
-// 注册的模块的抛出的类型
+// 注册的模块的抛出的类型 
 declare const _default: (app: any) => void
 export default _default
 // 组件库内的图标资源
