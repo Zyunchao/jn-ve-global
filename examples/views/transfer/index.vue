135139d741b040ea4ddeefdd04bdea3126882c88
Zyunchao <18651805393@163.com>
2023-03-03 13:53:12 +0800

    test: 穿梭框测试增加属性穿梭框；

--- examples/views/transfer/index.vue
+++ examples/views/transfer/index.vue
@@ -8,6 +8,10 @@
         :total="total"
         @pagination-change="handlePaginationChange"
     ></g-transfer>
+
+    <hr style="margin: 20px 0" />
+
+    <GTransferTree v-model="treeSelectKeys" :data="treeData" />
 </template>
 
 <script lang="ts">
@@ -19,6 +23,8 @@ export default defineComponent({
 
 <script lang="ts" setup>
 import { watch, ref, computed, reactive, onMounted, onUnmounted, nextTick } from 'vue'
+import mockOrgData from '../selectTreeTest/orgData.json'
+import _ from 'lodash'
 
 interface Option {
     key: number
@@ -26,9 +32,19 @@ interface Option {
     disabled?: boolean
 }
 
+const treeData = ref(_.cloneDeep(mockOrgData))
+const treeSelectKeys = ref<Array<string | number>>(['1615264570391699457', '1235'])
+
 const sourceData = ref<Option[]>(generateData())
 const selectKeys = ref<Array<string | number>>([])
 const total = ref<number>(100)
+
+watch(
+    () => treeSelectKeys.value,
+    (list) => {
+        console.log(`%c 外部的打印选中的值 ...................`, 'color: #f56c6c;', list)
+    }
+)
 
 watch(
     () => selectKeys.value,
@@ -55,6 +71,22 @@ function generateData(currentPage: number = 1, pageSize: number = 10) {
 
     return data
 }
+
+// 处理模拟树数据
+function machineData(treeData: any[]) {
+    treeData.forEach((item) => {
+        if (item.children && item.children.length) {
+            machineData(item.children)
+        }
+
+        if (item.type === '10') {
+            // console.log(`%c item.type === `, 'color: #e6a23c;', item.type)
+            item.disabled = true
+        }
+    })
+}
+
+machineData(treeData.value)
 </script>
 
 <style lang="scss" scoped></style>
