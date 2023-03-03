33dd3cb442455f2a6a4f6f5a19bfab8d5abbd804
Zyunchao <18651805393@163.com>
2023-03-03 13:52:34 +0800

    feat: 类型抛出；

--- packages/GSelectTree/v1/index.ts
+++ packages/GSelectTree/v1/index.ts
@@ -1,3 +1,5 @@
 export * from './interface/TreeData'
 export { default as SelectTreeTreeProps } from './interface/TreeProps'
 export { default as SelectTreeTreeConfig } from './interface/TreeConfig'
+export { default as TreeV2Config } from './interface/TreeV2Config'
+export { default as TreeV2Props } from './interface/TreeV2Props'
